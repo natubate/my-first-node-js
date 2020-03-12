994ffdd8a4aeb671ea65a8a59dd9185cfc5edbad
natubate <natubateaik@gmail.com>
2020-03-12 17:16:46 +0900

    first commit

---
+++ sum/index.js
@@ -0,0 +1,10 @@
+function add(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+      result = result + num;
+    }
+    return result;
+  }
+  module.exports = {
+    add : add
+  };
